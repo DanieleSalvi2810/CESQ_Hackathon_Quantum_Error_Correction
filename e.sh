1a603e7e6bc5cfe2b24ae04b8db719537cf95f8c
FedericoQuartieri <federico.quartieri2002@gmail.com>
2026-04-01 13:23:56 +0200

    modified README

---
+++ e.sh
@@ -0,0 +1,62 @@
+/usr/bin/convert -delay 70 -loop 0 \
+  assets/readme/tmp/slide-7.png \
+  assets/readme/tmp/slide-8.png \
+  assets/readme/tmp/slide-9.png \
+  assets/readme/tmp/slide-10.png \
+  assets/readme/gifs/syndromes_pairs.gif
+
+/usr/bin/convert -delay 90 -loop 0 \
+  assets/readme/tmp/slide-11.png \
+  assets/readme/tmp/slide-12.png \
+  assets/readme/gifs/infer_error.gif
+
+/usr/bin/convert -delay 90 -loop 0 \
+  assets/readme/tmp/slide-15.png \
+  assets/readme/tmp/slide-16.png \
+  assets/readme/gifs/trivial_loops.gif
+
+/usr/bin/convert -delay 90 -loop 0 \
+  assets/readme/tmp/slide-17.png \
+  assets/readme/tmp/slide-18.png \
+  assets/readme/tmp/slide-19.png \
+  assets/readme/gifs/nontrivial_loops.gif
+
+/usr/bin/convert -delay 90 -loop 0 \
+  assets/readme/tmp/slide-20.png \
+  assets/readme/tmp/slide-21.png \
+  assets/readme/tmp/slide-22.png \
+  assets/readme/tmp/slide-23.png \
+  assets/readme/gifs/toric_2qubits.gif
+
+/usr/bin/convert -delay 80 -loop 0 \
+  assets/readme/tmp/slide-29.png \
+  assets/readme/tmp/slide-30.png \
+  assets/readme/tmp/slide-31.png \
+  assets/readme/tmp/slide-32.png \
+  assets/readme/gifs/tailored_error_chain.gif
+
+/usr/bin/convert -delay 90 -loop 0 \
+  assets/readme/tmp/slide-33.png \
+  assets/readme/tmp/slide-34.png \
+  assets/readme/tmp/slide-35.png \
+  assets/readme/tmp/slide-36.png \
+  assets/readme/gifs/biased_matching.gif
+
+/usr/bin/convert -delay 90 -loop 0 \
+  assets/readme/tmp/slide-41.png \
+  assets/readme/tmp/slide-42.png \
+  assets/readme/tmp/slide-43.png \
+  assets/readme/tmp/slide-44.png \
+  assets/readme/gifs/erasures_recovery.gif
+
+/usr/bin/convert -delay 90 -loop 0 \
+  assets/readme/tmp/slide-45.png \
+  assets/readme/tmp/slide-46.png \
+  assets/readme/tmp/slide-47.png \
+  assets/readme/tmp/slide-48.png \
+  assets/readme/gifs/erasure_hardware.gif
+
+/usr/bin/convert -delay 120 -loop 0 \
+  assets/readme/tmp/slide-49.png \
+  assets/readme/tmp/slide-50.png \
+  assets/readme/gifs/erasure_failure.gif
